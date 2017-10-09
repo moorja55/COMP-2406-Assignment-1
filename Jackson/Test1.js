c1c9d3f84240a8e43874ada1954bdbe52ec7149b
Jackson Moore <JacksonMoore012@gmail.com>
2017-10-08 22:03:02 -0400

    Fixed spacing in test

--- Jackson/Test1.js
+++ Jackson/Test1.js
@@ -9,56 +9,51 @@ var counter = 1000;
 
 http.createServer(function (request,response){
 
-  console.log("url: " + request.url);
-  console.log("method: " + request.method);
+	console.log("url: " + request.url);
+	console.log("method: " + request.method);
 
-  /*
-  console.log("request headers:");
-  console.log(request.headers);
-  for(k in request) if(request.hasOwnProperty(k)) console.log("key: " + k);
-  */
 
-   //respond to client
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.write('Hello\n');
-   var urlString = request.url;
-   var index = urlString.indexOf("?name=");
-   if(index > 0) {
-     var name = urlString.substring(index + "?name=".length, urlString.length);
-     response.write(name + "\n");
-   }
-   else response.write('World\n');
+	//respond to client
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write('Hello\n');
+	var urlString = request.url;
+	var index = urlString.indexOf("?name=");
+	if(index > 0) {
+		var name = urlString.substring(index + "?name=".length, urlString.length);
+		response.write(name + "\n");
+	}
+	else response.write('World\n');
+	
+	function read(callback) {
+		setTimeout(function(){
+		var fs = require('fs');
+		fs.readFile('songs/Sister Golden Hair.txt', function(err, data) {
+			if(err) throw err;
+			var array = data.toString().split("\n");
+			for(var i=0; i<array.length; i++) {response.write(array[i]); }
+			//for(var i=0; i<array.length; i++) { console.log(array[i]); }
+			response.write('\n');
+		});
+		console.log("DONE");
+		console.log("read data");
+		callback();
+		}
+		, Math.floor((Math.random() * 1000) + 1));
+	}
 
-   function read(callback) {
-     setTimeout(function(){
-       var fs = require('fs');
-       fs.readFile('songs/Sister Golden Hair.txt', function(err, data) {
-         if(err) throw err;
-         var array = data.toString().split("\n");
-         for(var i=0; i<array.length; i++) {response.write(array[i]); }
-         //for(var i=0; i<array.length; i++) { console.log(array[i]); }
-         response.write('\n');
-       });
-       console.log("DONE");
-       console.log("read data");
-       callback();
-     }
-     , Math.floor((Math.random() * 1000) + 1));
-   }
+	read(function () {
+		setTimeout(function procces(){
+		console.log("process data");
+		setTimeout(function output (){
+			console.log("output data");
 
-   read(function () {
-     setTimeout(function procces(){
-   	  console.log("process data");
-   	  setTimeout(function output (){
-        console.log("output data");
-
-        //end HTTP response and provide final data to send
-        response.end("["+ counter++ + "]\n");
-   				},
-          Math.floor((Math.random() * 1000) + 1));
+			//end HTTP response and provide final data to send
+			response.end("["+ counter++ + "]\n");
+			},
+			Math.floor((Math.random() * 1000) + 1));
         },
         Math.floor((Math.random() * 1000) + 1));
-   });
+	});
 
 
 }).listen(3000);
